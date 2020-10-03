127d823167fe67eb33642f926f41ed762f570b0f
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-10-03 14:06:41 +0200

    Add Lerna
    Co-authored-by: Aleksandra Sikora <ola.zxcvbnm@gmail.com>

---
+++ packages/app/app/components/Table.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { HotTable } from "@handsontable/react";
+import "handsontable/dist/handsontable.full.css";
+
+export const Table = () => {
+  const data = [
+    ["", "Ford", "Volvo", "Toyota", "Honda"],
+    ["2016", 10, 11, 12, 13],
+    ["2017", 20, 11, 14, 13],
+    ["2018", 30, 15, 12, 13],
+  ];
+
+  return (
+    <div id="hot-app">
+      <HotTable
+        data={data}
+        colHeaders={true}
+        rowHeaders={true}
+        width="600"
+        height="300"
+      />
+    </div>
+  );
+};
---
+++ todo.md
@@ -0,0 +1,18 @@
+# Proof of Concept
+
+- [ ] add Handsontable @Aleksandra
+- [ ] add Monaco @Piotr
+- [ ] wire them together
+
+# Workspace Chores
+
+- [ ] Use changesets
+
+# Backlog
+
+- [ ] Dark mode
+- [ ] Plugins
+
+# Someday/Maybe
+
+- [ ] Run savant from terminal with `npx savant`
